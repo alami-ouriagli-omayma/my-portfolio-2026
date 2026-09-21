60fbdfc34010b4bf8a139ebf67e716e2ce9aeac5
ALAMI OURIAGLI OMAYMA <oumaimaaloryagli@gmail.com>
2026-09-21 14:03:52 -0700

    Update Vite base path for portfolio deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  vite: { base: "" },
+  vite: { base: "/my-portfolio-2026/"},
   tanstackStart: {
     server: { entry: "server" },
     prerender: {
